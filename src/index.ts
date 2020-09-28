408a31978d1d91a1322ef23b20ccc2c366245a27
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2020-09-28 21:51:31 +0100

    cb temporary test added.

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import express from "express";
+import nock from "nock";
+import axios from "axios";
+
+import { CircuitBreaker } from "./CircuitBreaker";
+
+nock("http://localhost:3001")
+  .get("/resource?id=1")
+  .reply(200, "resource found")
+  .persist();
+
+nock("http://localhost:3001")
+  .get("/resource?id=2")
+  .reply(404, "resource not found")
+  .persist();
+
+const cb = new CircuitBreaker({
+  failureRateThreshold: 50,
+  waitDurationInOpenState: 10_000,
+  permittedNumberOfCallsInHalfOpenState: 10,
+  slidingWindowSize: 20,
+});
+
+const app = express();
+
+app.get("/api", async (req, res) => {
+  try {
+    const id = req.query["id"];
+    console.log("fetching resource ID: ", id);
+    const downstreamCallback = () =>
+      axios.get(`http://localhost:3001/resource?id=${id}`);
+    const { data } = await cb.exec(downstreamCallback);
+    console.log(data);
+    res.send(data);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/cbstate", (req, res) => {
+  res.send(cb.getState()?.state);
+});
+
+app.listen(3000, () => console.log("server started on port 8080"));
