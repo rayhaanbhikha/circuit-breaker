83df56fb36f24194667a0149c2e9422c6b3fa324
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2020-09-29 15:53:44 +0100

    e2e test removed setTimeout side effect

--- e2e/circuitbreaker.local.spec.ts
+++ e2e/circuitbreaker.local.spec.ts
@@ -4,6 +4,8 @@ describe("LOCAL Circuit breaker", () => {
   let cb: CircuitBreaker;
 
   beforeEach(() => {
+    jest.useFakeTimers();
+
     cb = new CircuitBreaker({
       failureRateThreshold: 50,
       waitDurationInOpenState: 10_000,
@@ -35,8 +37,6 @@ describe("LOCAL Circuit breaker", () => {
   });
 
   it("should transition circuit breaker to HALF_OPEN state after 10s", async () => {
-    jest.useFakeTimers();
-
     for (let i = 0; i < 15; i++)
       await cb
         .exec(jest.fn().mockRejectedValue("some-error"))
@@ -49,7 +49,6 @@ describe("LOCAL Circuit breaker", () => {
 
     jest.runTimersToTime(10_000);
     const { state } = await cb.currentStateManager.getState();
-
     expect(state).toEqual("HALF_OPEN");
   });
 });
