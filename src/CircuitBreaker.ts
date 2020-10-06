66994e39e07d5b1599dea6e22fb725e6bc835359
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2020-10-06 21:04:00 +0100

    distributed state added as conditional

--- src/CircuitBreaker.ts
+++ src/CircuitBreaker.ts
@@ -5,6 +5,7 @@ import {
 import { LocalState } from "./state-manager/internal/LocalState";
 import { CircuitBreakerMetrics } from "./metrics/CircuitBreakerMetrics";
 import { CircuitBreakerStateManager } from "./state-manager/CircuitBreakerStateManager";
+import { DistributedState } from "./state-manager/distributed/DistributedState";
 
 export class CircuitBreaker {
   readonly config: CircuitBreakerConfig;
@@ -14,7 +15,14 @@ export class CircuitBreaker {
   constructor(configOptions: ICircuitBreakerConfig) {
     this.config = new CircuitBreakerConfig(configOptions);
     this.metrics = new CircuitBreakerMetrics(this.config);
-    this.currentStateManager = new LocalState(this.config, this.metrics);
+    if (configOptions.distributedStateConfig) {
+      this.currentStateManager = new DistributedState(
+        this.config,
+        this.metrics
+      );
+    } else {
+      this.currentStateManager = new LocalState(this.config, this.metrics);
+    }
   }
 
   async exec(cb: Function) {
